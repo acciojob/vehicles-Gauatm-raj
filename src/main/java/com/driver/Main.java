9cc7ee2a97be026cc7aedc5f226f10c76de21de7
Gauatm-raj <gautamraj7569@gmail.com>
2024-01-19 21:45:06 +0530

    done done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,9 +3,12 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
+
         mercedes.move(40, 0);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
+        mercedes.steer(30);
+        mercedes.accelerate(129);
 
     }
 }
